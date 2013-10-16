c43ef7b979fc26c678df6cb9a8f77b79f0f23704
Keijiro Takahashi <keijiro@MSX.local>
2013-10-16 10:09:39 +0900

    Audio queue handler.

--- AudioSpectrum/SpectrumView.m
+++ AudioSpectrum/SpectrumView.m
@@ -1,4 +1,20 @@
 #import "SpectrumView.h"
+#import <AudioToolbox/AudioToolbox.h>
+
+static AudioQueueRef queue;
+static AudioQueueBufferRef buffers[4];
+
+static float waveform[512];
+
+static void HandleInputBuffer(void *aqData, AudioQueueRef inAQ, AudioQueueBufferRef inBuffer, const AudioTimeStamp *inStartTime, UInt32 inNumPackets, const AudioStreamPacketDescription *inPacketDesc)
+{
+    const float *samples = (const float *)inBuffer->mAudioData;
+    for (int i = 0; i < 512 && i < inNumPackets; i++) {
+        waveform[i] = samples[i];
+    }
+    
+    AudioQueueEnqueueBuffer(queue, inBuffer, 0, nil);
+}
 
 @implementation SpectrumView
 
@@ -6,7 +22,25 @@
 {
     self = [super initWithFrame:frame];
     if (self) {
-        [NSTimer scheduledTimerWithTimeInterval:(1.0f / 20) target:self selector:@selector(redraw) userInfo:nil repeats:YES];
+        AudioStreamBasicDescription inFormat = {0};
+        inFormat.mFormatID = kAudioFormatLinearPCM;
+        inFormat.mSampleRate = 44100;
+        inFormat.mChannelsPerFrame = 1;
+        inFormat.mBitsPerChannel = 32;
+        inFormat.mBytesPerPacket = inFormat.mBytesPerFrame = inFormat.mChannelsPerFrame * sizeof(AudioSampleType);
+        inFormat.mFramesPerPacket = 1;
+        inFormat.mFormatFlags = kAudioFormatFlagsCanonical;
+        
+        AudioQueueNewInput(&inFormat, HandleInputBuffer, nil, nil, kCFRunLoopCommonModes, 0, &queue);
+        
+        for (int i = 0; i < 4; i++) {
+            AudioQueueAllocateBuffer(queue, 1024, &buffers[i]);
+            AudioQueueEnqueueBuffer(queue, buffers[i], 0, nil);
+        }
+        
+        AudioQueueStart(queue, nil);
+        
+        [NSTimer scheduledTimerWithTimeInterval:(1.0f / 100) target:self selector:@selector(redraw) userInfo:nil repeats:YES];
     }
     return self;
 }
@@ -23,10 +57,10 @@
     NSSize size = self.frame.size;
     
     NSBezierPath* path = [NSBezierPath bezierPath];
-    [path moveToPoint:NSMakePoint(0, 0)];
-    for (int i = 0; i < 128; i++) {
-        float x = (float)i * size.width / 128;
-        float y = arc4random() % (int)size.height;
+    [path moveToPoint:NSMakePoint(0, size.height * 0.5f)];
+    for (int i = 0; i < 512; i++) {
+        float x = (float)i * size.width / 512;
+        float y = (waveform[i] + 1.0f) * 0.5f * size.height;
         [path lineToPoint:NSMakePoint(x, y)];
     }
     
