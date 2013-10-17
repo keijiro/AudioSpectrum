4e67097cc2213afb9a59a0bc5b71f84edd819bce
Keijiro Takahashi <keijiro@gmail.com>
2013-10-17 01:05:13 -0700

    FFT test.

--- AudioSpectrum/SpectrumView.m
+++ AudioSpectrum/SpectrumView.m
@@ -7,14 +7,14 @@
 {
     self = [super initWithFrame:frame];
     if (self) {
-        fftSetup = vDSP_create_fftsetup(8, FFT_RADIX2); 
-        fftBuffer.realp = malloc(256 * sizeof(float));
-        fftBuffer.imagp = malloc(256 * sizeof(float));
+        fftSetup = vDSP_create_fftsetup(10, FFT_RADIX2);
+        fftBuffer.realp = malloc(512 * sizeof(float));
+        fftBuffer.imagp = malloc(512 * sizeof(float));
 
         self.audioBuffer = [[AudioInputBuffer alloc] init];
         [self.audioBuffer start];
 
-        [NSTimer scheduledTimerWithTimeInterval:(1.0f / 20) target:self selector:@selector(redraw) userInfo:nil repeats:YES];
+        [NSTimer scheduledTimerWithTimeInterval:(1.0f / 60) target:self selector:@selector(redraw) userInfo:nil repeats:YES];
     }
     return self;
 }
@@ -28,20 +28,30 @@
 {
 	[super drawRect:dirtyRect];
 
-    float samples[256];
-    NSUInteger sampleCount = [self.audioBuffer copyWaveformTo:samples length:256];
+    float samples[1024];
+    NSUInteger sampleCount = [self.audioBuffer copyWaveformTo:samples length:1024];
+    
+    if (sampleCount != 1024) return;
+    
+    vDSP_ctoz((DSPComplex*)samples, 2, &fftBuffer, 1, 512);
+    
+    vDSP_fft_zrip(fftSetup, &fftBuffer, 1, 10, FFT_FORWARD);
+    
+    for (int i = 0; i < 512; i++) {
+        samples[i] = sqrtf(fftBuffer.realp[i] * fftBuffer.realp[i] + fftBuffer.imagp[i] * fftBuffer.imagp[i]);
+    }
     
     NSSize size = self.frame.size;
     NSBezierPath* path = [NSBezierPath bezierPath];
-    [path moveToPoint:NSMakePoint(0, size.height * 0.5f)];
+    [path moveToPoint:NSMakePoint(0, 0)];
 
-    for (int i = 0; i < sampleCount; i++) {
-        float x = (float)i * size.width / sampleCount;
-        float y = (samples[i] + 1.0f) * 0.5f * size.height;
+    for (int i = 0; i < 512; i++) {
+        float x = (float)i * size.width / 512;
+        float y = samples[i] * 0.5f * size.height;
         [path lineToPoint:NSMakePoint(x, y)];
     }
     
-    if (sampleCount > 0) [path stroke];
+    [path stroke];
 }
 
 @end
